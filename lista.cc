a67c82e10e7c0bdfec799a5026b8c8bb7dc930cd
FlechitUp <ximena.pocco@ucsp.edu.pe>
2016-09-15 09:08:40 -0500

    x

--- lista.cc
+++ lista.cc
@@ -6,7 +6,7 @@ int main()
 {
 	cout<<"hola mundo!!!!"<<endl;
 	cout<<"hello word!!"<<endl;
-	cout<<"when the night"<<endl;
+	cout<<"when the nigh lalalalallalala"<<endl;
 	cout<<"escribiendo...jsdfj"<<endl;
 	return 0;
 }
